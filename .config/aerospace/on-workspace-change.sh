15603ef8f68615c1edf6ed0e8cab8bd1d4d1bcd8
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2026-06-03 10:09:28 +0530

    Auto-update dotfiles: 2026-06-03 10:09:28

--- .config/aerospace/on-workspace-change.sh
+++ .config/aerospace/on-workspace-change.sh
@@ -6,7 +6,3 @@
 # Trigger SketchyBar update
 sketchybar --trigger aerospace_workspace_change FOCUSED_WORKSPACE="$AEROSPACE_FOCUSED_WORKSPACE"
 
-# Automatically open Zotero only when visiting the Research workspace
-if [ "$AEROSPACE_FOCUSED_WORKSPACE" = "Research" ]; then
-    open -a Zotero
-fi
