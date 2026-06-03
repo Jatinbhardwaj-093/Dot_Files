391bea08329fbebdc0275bb058033e038a4f5873
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2026-06-03 11:24:44 +0530

    Auto-update dotfiles: 2026-06-03 11:24:44

--- vim_mappings.lua
+++ vim_mappings.lua
@@ -24,7 +24,7 @@ vim.keymap.set('n', '<S-h>', ':bprevious<CR>')
 
 -- terminal
 vim.keymap.set('n', '<leader>t', ':terminal<CR>')
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+vim.keymap.set('t', '<Esc><Esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- Comment toggle keymaps (Cmd+/ translated via Ctrl+/ and Ctrl+_)
 map("n", "<C-_>", function() require("Comment.api").toggle.linewise.current() end, { desc = "Comment toggle line" })
